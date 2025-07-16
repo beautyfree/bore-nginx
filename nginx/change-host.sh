94fc4f7c0997c3b1869cf0e8dfcc7d531a9f2894
Alexey Elizarov <felex-ae@ya.ru>
2025-07-16 22:34:23 +0300

    feat: update

--- nginx/change-host.sh
+++ nginx/change-host.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo "Changing host to ${BORE_HOST}"
+echo "Configuring nginx with BORE_HOST=${BORE_HOST:-bore-server} and DNS_RESOLVER=${DNS_RESOLVER:-127.0.0.11}"
+
 dockerize -template /etc/nginx/conf.d/default.tpl:/etc/nginx/conf.d/default.conf \
           -template /etc/nginx/bore-server.tpl:/etc/nginx/bore-server.conf \
-          -wait tcp://$BORE_HOST:7835 -timeout 60s
+          -wait tcp://${BORE_HOST:-bore-server}:7835 -timeout 60s
